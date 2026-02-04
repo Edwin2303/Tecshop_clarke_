dee1f19193deca1738dd06296ed8dc79ded4bfb4
Edwin2303 <136734546+Edwin2303@users.noreply.github.com>
2026-02-04 17:50:57 -0600

    commit#2

---
+++ src/main/java/com/Store/Tecshop/TecshopApplication.java
@@ -0,0 +1,13 @@
+package com.Store.Tecshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecshopApplication.class, args);
+	}
+
+}
